df1ca49d6fd6d8570d4ece4247878395652b466b
fox <n.zales99@gmail.com>
2018-09-30 21:57:50 +0300

    part 1 news

---
+++ app/src/main/java/com/fox/academy_lesson1/NewListEx/AdapterNewList.java
@@ -0,0 +1,8 @@
+package com.fox.academy_lesson1.NewListEx;
+
+/**
+ * Created by fox on 30.09.18.
+ */
+
+public class AdapterNewList {
+}
---
+++ app/src/main/java/com/fox/academy_lesson1/NewListEx/NewsList.java
@@ -0,0 +1,15 @@
+package com.fox.academy_lesson1.NewListEx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fox.academy_lesson1.R;
+
+public class NewsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
