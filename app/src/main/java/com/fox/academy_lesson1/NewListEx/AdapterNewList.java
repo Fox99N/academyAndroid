99c3f752d820503dd7fb305f5908e8c7ebc8b00d
fox <n.zales99@gmail.com>
2018-10-08 22:46:01 +0300

    part2

--- app/src/main/java/com/fox/academy_lesson1/NewListEx/AdapterNewList.java
+++ app/src/main/java/com/fox/academy_lesson1/NewListEx/AdapterNewList.java
@@ -1,9 +1,12 @@
 package com.fox.academy_lesson1.NewListEx;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,50 +19,79 @@ import java.util.List;
  * Created by fox on 30.09.18.
  */
 
-public class AdapterNewList extends BaseAdapter{
+public class AdapterNewList extends RecyclerView.Adapter<AdapterNewList.ViewHolder> {
     private final List<NewsItem> news;
     private final Context context;
     private final LayoutInflater layoutInflater;
+    private ItemClickListener clickListener;
 
     public AdapterNewList(List<NewsItem> news, Context context) {
         this.news = news;
         this.context = context;
         this.layoutInflater = (LayoutInflater) context.getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
-        ;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView image;
+        TextView author;
+        TextView textHeader;
+        TextView textDescription;
+        TextView textData;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.news_img);
+            author = itemView.findViewById(R.id.news_author_txt);
+            textHeader = itemView.findViewById(R.id.news_header_txt);
+            textDescription = itemView.findViewWithTag(R.id.news_descriptions_txt);
+            textData = itemView.findViewById(R.id.news_data_txt);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (clickListener != null) clickListener.onItemClick(itemView, getAdapterPosition());
+
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.new_items_list, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
-    public int getCount() {
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        NewsItem newsItem = news.get(position);
+        holder.image.setImageResource(Integer.parseInt(newsItem.getImageUrl()));
+        holder.author.setText((CharSequence) newsItem.getCategory()); //FIXME скорее всего некорретно откастует, опнять почему к чару
+        holder.textHeader.setText(newsItem.getTitle());
+        holder.textDescription.setText(newsItem.getFullText());
+        holder.textData.setText((CharSequence) newsItem.getPublishDate());
+
+    }
+
+
+    @Override
+    public int getItemCount() {
         return news.size();
     }
 
-    @Override
-    public NewsItem getItem(int position) {
-        return news.get(position);
+    NewsItem getItem(int id){
+        return news.get(id);
     }
 
-    @Override
-    public long getItemId(int position) {
-        return 0;
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View rowItem = layoutInflater.inflate(R.layout.new_items_list, parent, false);
-        ImageView image = rowItem.findViewById(R.id.news_img);
-        TextView author = rowItem.findViewById(R.id.news_author_txt);
-        TextView textHeader = rowItem.findViewById(R.id.news_header_txt);
-        TextView textDescription = rowItem.findViewWithTag(R.id.news_descriptions_txt);
-        TextView textData = rowItem.findViewById(R.id.news_data_txt);
 
-        NewsItem newsItem= news.get(position);
-        image.setImageResource(Integer.parseInt(newsItem.getImageUrl()));
-        author.setText((CharSequence) newsItem.getCategory()); //FIXME скорее всего некорретно откастует, опнять почему к чару
-        textHeader.setText(newsItem.getTitle());
-        textDescription.setText(newsItem.getFullText());
-        textData.setText((CharSequence) newsItem.getPublishDate());
-        return rowItem;
-    }
-
+    //TODO доделать holder и все под ресайкл с 100 страницы
 }
